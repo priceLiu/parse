a16906779c9c23a6264b2e5ea9c71739cac8cbf7
price.liu <liu_price168@126.com>
2013-11-01 16:24:07 +0800

    added dal model

---
+++ MySpider/MySpider.DAL.Model/IArticleDBModel.cs
@@ -0,0 +1,76 @@
+﻿using Peanut.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpider.DAL.Model
+{
+    [Table("Article")]
+    interface IArticleDBModel
+    {
+        [ID]
+        int Id
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        string Title
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        string Summary
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        bool IsRecommend
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        string NavUrl
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        int StarCount
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        int BrowsedCount
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        string Posted
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        DateTime Created
+        {
+            get;
+            set;
+        }
+    }
+}
