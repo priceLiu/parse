1413bef0455c35ac1a22da7f2e709c50833a7b93
price.liu <liu_price168@126.com>
2013-11-01 16:54:44 +0800

    added bll model

---
+++ MySpider/MySpider.BLL.Model/ArticleModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpider.BLL.Model
+{
+    public class ArticleModel
+    {
+        public ArticleType Type
+        {
+            get;
+            set;
+        }
+
+        public ImgLinkModel ImgLink
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum ArticleType
+    {
+        Maintain = 1,       //养护
+        Cosmetology = 2,    //美容
+        Repair = 3,         //维修
+        Remodel = 4,        //改装
+        Safety = 5,         //保险
+        Appliance = 6       //用品
+    }
+}
---
+++ MySpider/MySpider.BLL.Model/ImgLinkModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpider.BLL.Model
+{
+    public class ImgLinkModel
+    {
+        public class Attributes
+        {
+            public const string ALT = "alt";
+            public const string SRC = "src";
+        }
+
+        public string Src
+        {
+            get;
+            set;
+        }
+
+        public string NavigateUrl
+        {
+            get;
+            set;
+        }
+
+        public string Alt
+        {
+            get;
+            set;
+        }
+    }
+}
